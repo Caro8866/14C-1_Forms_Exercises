de7d7ce04c012e56b41a3c9a1a6b1694717dab0a
Caroline Thostrup <caro342h@stud.kea.dk>
2022-10-25 08:24:23 +0200

    init supabaseSandBox ex

---
+++ supabaseSandBox/app.js
@@ -0,0 +1,8 @@
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRydnNqZnlrY29mbG5lYmtkYWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg4NjEsImV4cCI6MTk4MjE3NDg2MX0.IsHFJrPbfFh6cVBiDXBYzebe7Fx_qULBpYEPyFVu_eY;
+
+/* curl 'https://drvsjfykcoflnebkdajh.supabase.co/rest/v1/tvshows?select=id' \
+-H "apikey: SUPABASE_KEY" \
+-H "Authorization: Bearer SUPABASE_KEY" */
+
+const url = "https://drvsjfykcoflnebkdajh.supabase.co/rest/v1/tvshows?select=id";
+const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRydnNqZnlrY29mbG5lYmtkYWpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY1OTg4NjEsImV4cCI6MTk4MjE3NDg2MX0.IsHFJrPbfFh6cVBiDXBYzebe7Fx_qULBpYEPyFVu_eY;";
